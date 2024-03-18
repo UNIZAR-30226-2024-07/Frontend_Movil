09c67918c697f01b4d3af83aa40a10359cf70f79
Naacho5 <149486119+Naacho5@users.noreply.github.com>
2024-03-18 15:17:33 +0100

    Creado fichero colores.dart

---
+++ lib/colores.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColoresApp {
+  static const Color fondoPantallaColor = Color(0xE8539165);
+  static const Color cabeceraColor = Color(0xE79E6E83);
+  static const Color fondoPantallaColor2 = Color(0xFF5FB763);
+  static const Color segundoColor = Color(0xE83F497F);
+  static const Color tercerColor = Color(0xFFF7C04A);
+  static const Color cuartoColor = Color(0xFFF8F5E4);
+}
